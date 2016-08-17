74e00ccf7411363b3930c31c102a280a55dc2957
Tim Hardy <tim.hardy@omegacreations.com>
2016-08-17 08:44:08 -0500

    Working on template include bug

--- server/src/templates/templateEngine.js
+++ server/src/templates/templateEngine.js
@@ -25,12 +25,15 @@ var TemplateEngine = function(args) {
         return templateEngine.getTemplate(path)
             .then((templateObject) => {
                 if (templateObject && templateObject.template) {
+                    // todo: return template object and figure out why I wasn't doing that before
+                    return templateObject;
+
                     // todo: I suspect that my returning of only the templates here and not the model that goes with it
                     //  is causing all of my includes to not render properly. e.g. RegenerateTemplate-Navigation references
                     //  navItems, which the parsing engine won't have a clue where that comes from when used in an include.
                     //  Has liquid-node been updated to accept a model with an include?  Just a template is not enough.  Every
                     //  include needs its model that goes with it, the entire templateObject.
-                    return templateObject.template; // return the template itself
+                    //return templateObject.template; // return the template itself
                 }
                 else {
                     throw new Error(`template '${path}' not found`);
@@ -41,7 +44,7 @@ var TemplateEngine = function(args) {
 
     templateEngine.engine.fileSystem = new CustomFileSystem();
 
-
+    // todo: alter this to accept a templateObject instead of a templateString
     templateEngine.Render = function(templateString, model) {
         var renderPromise = templateEngine.engine.parse(templateString)
             .then((template) => {
