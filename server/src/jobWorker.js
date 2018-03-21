b5d74af4a81c846b91c09f625921ca30f0b34c1d
Tim Hardy <tim.hardy@omegacreations.com>
2018-03-20 23:18:40 -0500

    split scheduled jobs out from web server

--- server/src/jobWorker.js
+++ server/src/jobWorker.js
@@ -1,2 +1,35 @@
-require('./scheduling/agenda.js');
+const config = require('./server/config');
+const moment = require('moment');
+
+const agenda = require('./schedules/agenda.js');
+
+//var jobTypes = process.env.JOB_TYPES ? process.env.JOB_TYPES.split(',') : [];
+const jobTypes = config.jobTypes ? config.jobTypes.split(',') : [];
+
+jobTypes.forEach(function(type) {
+    require('./schedules/jobs/' + type)(agenda);
+});
+
+agenda.on('ready', function() {
+    if (jobTypes.length) {
+        agenda.start();
+    }
+});
+
+agenda.on('start', function(job) {
+    console.log("Job %s starting", job.attrs.name);
+});
+
+agenda.on('complete', function(job) {
+    console.log(`Job %s finished at ${moment().format('MM-DD-YYYY hh:mm:ss')}`, job.attrs.name);
+});
+
+agenda.on('success:regenerateJob', function(job) {
+    console.log("regenerateJob Success for orgId: %s", job.attrs.data.orgId);
+});
+
+agenda.on('fail:regenerateJob', function(err, job) {
+    console.log("Job failed with error: %s", err.message);
+});
+
 
--- server/src/schedules/agenda.js
+++ server/src/schedules/agenda.js
@@ -1,37 +1,7 @@
 const Agenda = require('agenda');
 const config = require('../server/config');
-const moment = require('moment');
 
 // todo: split this export out from the actual scheduling stuff below it
 const agenda = new Agenda({db: {address: config.mongoDbUrl, collection: 'agendaJobs'}});
-
-//var jobTypes = process.env.JOB_TYPES ? process.env.JOB_TYPES.split(',') : [];
-const jobTypes = config.jobTypes ? config.jobTypes.split(',') : [];
-
-jobTypes.forEach(function(type) {
-    require('./jobs/' + type)(agenda);
-});
-
-agenda.on('ready', function() {
-    if (jobTypes.length) {
-        agenda.start();
-    }
-});
-
-agenda.on('start', function(job) {
-    console.log("Job %s starting", job.attrs.name);
-});
-
-agenda.on('complete', function(job) {
-    console.log(`Job %s finished at ${moment().format('MM-DD-YYYY hh:mm:ss')}`, job.attrs.name);
-});
-
-agenda.on('success:regenerateJob', function(job) {
-    console.log("regenerateJob Success for orgId: %s", job.attrs.data.orgId);
-});
-
-agenda.on('fail:regenerateJob', function(err, job) {
-    console.log("Job failed with error: %s", err.message);
-});
 
 module.exports = agenda;
