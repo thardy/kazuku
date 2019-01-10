c9b0f88426e338e61245ca10536cb9ef9c0288f4
Tim Hardy <tim.hardy@omegacreations.com>
2019-01-10 08:37:35 -0600

    in progress on making relationships work in graphQL

--- server/src/server/graphQL/schemaService.js
+++ server/src/server/graphQL/schemaService.js
@@ -1,3 +1,5 @@
+import {GraphQLList} from "graphql";
+
 const {GraphQLSchema, GraphQLObjectType, GraphQLID, GraphQLString, GraphQLFloat, GraphQLInt, GraphQLNonNull} = require('graphql');
 const {simpleQuery, simpleCreateMutation, simpleUpdateMutation, simpleDeleteMutation} = require('./graphql.helper');
 const {GraphQLDateTime} = require('graphql-iso-date');
@@ -6,7 +8,10 @@ const CustomDataService = require('../../customData/customDataService');
 const CustomSchemaService = require('../../customSchemas/customSchemaService');
 const _ = require("lodash");
 
+const typesThatNeedFurtherProcessing = ['$ref', 'array'];
+
 class SchemaService {
+
     constructor(database) {
         this.customDataService = new CustomDataService(database);
         this.customSchemaService = new CustomSchemaService(database);
@@ -106,23 +111,58 @@ class SchemaService {
         let orgCustomSchemas = [];
         return this.customSchemaService.getAll(orgId)
             .then((allCustomSchemas) => {
-                orgCustomSchemas = allCustomSchemas;
+                orgCustomSchemas = allCustomSchemas.map((orgCustomSchema) => {
+                    return this.mapJsonSchemaProperties(orgCustomSchema);
+                });
 
+                const allCustomDataTypeFields = {};
+                const jsonSchemaIdToNameMappings = {};
                 const contentTypeActions = [];
 
                 for (const customSchema of orgCustomSchemas) {
-                    const contentTypeAction = this.getGraphQLActionForCustomSchema(customSchema);
-                    contentTypeActions.push(contentTypeAction);
+                    const graphQLFriendlyTypeName = _.camelCase(customSchema.name.trim()); //.replace(/ /g, '');
+                    const customDataTypeFields = this.getCustomDataTypeFieldsForCustomSchema(customSchema);
+                    allCustomDataTypeFields[graphQLFriendlyTypeName] = customDataTypeFields;
                 }
 
-                const rootQueryFields = contentTypeActions.reduce((acc, action) => {
-                    acc[action.id] = action.query;
-                    return acc;
+                // todo: make the second pass here, once we've got customDataTypeFields for every customSchema for this org
+                // todo: keep looping until we didn't have to change anything <- is this necessary???
+                // loop through allCustomDataTypes
+                for (const customDataTypeName in allCustomDataTypeFields) {
+                    const customDataTypeFields = allCustomDataTypeFields[customDataTypeName];
+                    for (const customDataTypeName in customDataTypeFields) {
+                        //  whenever we see a customDataTypeFields[propertyName].type that starts with either 'array:' or '$ref:',
+                        const graphQLType = customDataTypeFields[customDataTypeName].type;
+
+                        if (graphQLType.startsWith('array:')) {
+
+                        }
+                        else if (graphQLType.startsWith('$ref:')) {
+
+                        }
+
+                        //  replace with...
+                        //  let predefinedTypeName = jsonSchemaIdToNameMappings[customDataTypeFields[propertyName].type]
+                        //  customDataTypeFields[propertyName].type = allCustomDataTypes[predefinedTypeName] or new GraphQLList(...) for arrays
+                        //graphQLType = new GraphQLList(allCustomDataTypes[customDataType.name])
+                    }
+
+
+
+
+                }
+
+
+
+
+                const rootQueryFields = contentTypeActions.reduce((actions, action) => {
+                    actions[action.id] = action.query;
+                    return actions;
                 }, {});
 
                 const rootQuery = new GraphQLObjectType({
                     name: 'root_query',
-                    fields: rootQueryFields
+                    fields: () => rootQueryFields
                 });
 
                 const rootMutation = new GraphQLObjectType({
@@ -145,8 +185,34 @@ class SchemaService {
             });
     }
 
-    getGraphQLActionForCustomSchema(customSchema) {
+    getGraphQLActionForCustomSchema(graphQLFriendlyTypeName, customDataTypeFields) {
+        const customDataType = new GraphQLObjectType({
+            name: graphQLFriendlyTypeName,
+            fields: () => (customDataTypeFields)
+        });
+
+        const contentTypeAction = {
+            id: graphQLFriendlyTypeName,
+            query: simpleQuery(customSchema.contentType, customDataType),
+            mutation: {
+                create: simpleCreateMutation(customSchema.contentType, customDataType),
+                update: simpleUpdateMutation(customSchema.contentType, customDataType),
+                delete: simpleDeleteMutation(customSchema.contentType, customDataType),
+            }
+        };
+
+        return contentTypeAction;
+    }
+
+    getCustomDataTypeFieldsForCustomSchema(customSchema, jsonSchemaIdToNameMappings) {
         const graphQLFriendlyTypeName = _.camelCase(customSchema.name.trim()); //.replace(/ /g, '');
+
+        if (customSchema['$id']) {
+            // map the jsonSchemaId to the friendly name - we will map any customSchema that comes through with an '$id' specified
+            // (e.g.  jsonSchemaIdToNameMappings['https://thanos.kazuku.com/api/jsonschemas/tags.schema.json'] = 'tags')
+            jsonSchemaIdToNameMappings[customSchema['$id']] = graphQLFriendlyTypeName;
+        }
+
         const customDataTypeFields = {
             _id: {
                 type: GraphQLID
@@ -173,53 +239,148 @@ class SchemaService {
 
         for (const property in customSchema.jsonSchema.properties) {
             const propertyName = _.camelCase(property);
-            const graphQLType = this.getGraphQLTypeForSchemaProperty(customSchema.jsonSchema.properties[property].type);
+            const jsonSchemaType = customSchema.jsonSchema.properties[property].type || customSchema.jsonSchema.properties[property]['$ref'];
+            let graphQLType = this.getGraphQLTypeForSchemaProperty(jsonSchemaType);
+
+            const needsFurtherProcessing = typesThatNeedFurtherProcessing.includes(graphQLType);
+
+            if (needsFurtherProcessing) {
+                switch(graphQLType) {
+                    case 'array':
+                        // example jason Schema for scalar array
+                        // "additionalName": {
+                        //     "type": "array",
+                        //     "items": {
+                        //         "type": "string"
+                        //     }
+                        // }
+                        // or..............
+                        // example json Schema for referential array
+                        // "tags" : {
+                        //     "type" : "array",
+                        //     "items" : {
+                        //         "$ref" : "https://thanos.kazuku.com/api/jsonschemas/tags.schema.json"
+                        //     },
+                        // }
+                        let typeInArray = '';
+                        if (customSchema.jsonSchema.properties[property].items.type) {
+                            graphQLType = new GraphQLList(customSchema.jsonSchema.properties[property].items.type);
+                        }
+                        else if (customSchema.jsonSchema.properties[property].items['$ref']) {
+                            // this will just be a placeholder.  We have to make a second pass after all other types are done to replace with
+                            // the actual customDataType object created for that type - something like...
+                            // graphQLType = new GraphQLList(allCustomDataTypes[customDataType.name]);
+                            graphQLType = `array:${customSchema.jsonSchema.properties[property].items['$ref']}`;
+                        }
+                        else {
+                            throw new Error(`Invalid array type found in jsonSchema for property ${property}.  'items' object must contain either 'type' or '$ref'`);
+                        }
+
+                        break;
+                    case '$ref':
+                        graphQLType = `ref:${customSchema.jsonSchema.properties[property].items['$ref']}`;
+                        break;
+                    default:
+                        throw new Error(`Unrecognized graphQLType fell into needsFurtherProcessing - ${graphQLType}`);
+                        break;
+                }
+            }
 
             customDataTypeFields[propertyName] = {
                 type: graphQLType
             };
+
         }
 
-        const customDataType = new GraphQLObjectType({
-            name: graphQLFriendlyTypeName,
-            fields: customDataTypeFields
-        });
+        // what an actual object looks like with relationships...
+        // const categoryType = new GraphQLObjectType({
+        //     name: 'CategoryType',
+        //     fields: () => ({
+        //         name: { type: GraphQLString },
+        //         children: { type: new GraphQLList(categoryType) }
+        //     })
+        // });
+        // const customDataTypeData = {
+        //     name: graphQLFriendlyTypeName,
+        //     fields: customDataTypeFields
+        // };
+        //
+        // // we will need all the customDataTypes easily accessible by name after we are done creating them all
+        // allCustomDataTypes[customDataType.name] = customDataTypeData;
 
-        const contentTypeAction = {
-            id: graphQLFriendlyTypeName,
-            query: simpleQuery(customSchema.contentType, customDataType),
-            mutation: {
-                create: simpleCreateMutation(customSchema.contentType, customDataType),
-                update: simpleUpdateMutation(customSchema.contentType, customDataType),
-                delete: simpleDeleteMutation(customSchema.contentType, customDataType),
-            }
-        };
-
-        return contentTypeAction;
+        return customDataTypeFields;
     }
 
     getGraphQLTypeForSchemaProperty(propertyType) {
         let graphQLType = GraphQLString;
 
-        switch(propertyType) {
-            case 'string':
-                graphQLType = GraphQLString;
-                break;
-            case 'number':
-                graphQLType = GraphQLInt;
-                break;
-            case 'string':
-                graphQLType = GraphQLDateTime;
-                break;
-            default:
-                graphQLType = GraphQLString;
-                break;
+        if (!propertyType.startsWith('https://')) {
+            switch(propertyType) {
+                case 'string':
+                    graphQLType = GraphQLString;
+                    break;
+                case 'number':
+                    graphQLType = GraphQLInt;
+                    break;
+                case 'string':
+                    graphQLType = GraphQLDateTime;
+                    break;
+                case 'array':
+                    graphQLType = 'array';
+                    break;
+                default:
+                    graphQLType = GraphQLString;
+                    break;
+            }
+            // todo: types to be implemented: GraphQLID, GraphQLFloat
         }
-        // todo: types to be implemented: GraphQLID, GraphQLFloat
+        else {
+            graphQLType = '$ref';
+        }
 
         return graphQLType;
     }
 
+    mapJsonSchemaProperties(orgCustomSchema) {
+        if (customSchema.jsonSchema) {
+            for (const property in customSchema.jsonSchema) {
+                if (jsonSchemaMappingKeys.includes(property)) {
+                    const translatedKey = this.translateJsonSchemaKey(property);
+                    customSchema.jsonSchema[translatedKey] = customSchema.jsonSchema[property];
+                    delete customSchema.jsonSchema[property];
+                }
+            }
+
+            if (customSchema.jsonSchema.properties) {
+                for (const propertyName in customSchema.jsonSchema.properties) {
+                    if (jsonSchemaMappingKeys.includes(propertyName)) {
+                        const translatedKey = this.translateJsonSchemaKey(propertyName);
+                        customSchema.jsonSchema.properties[translatedKey] = customSchema.jsonSchema.properties[propertyName];
+                        delete customSchema.jsonSchema.properties[propertyName];
+                    }
+                }
+            }
+        }
+
+        return schema;
+    }
+
+    translateJsonSchemaKey(jsonSchemaKey) {
+        let translatedKey = '';
+
+        switch(jsonSchemaKey) {
+            case 'jsonSchemaSchema':
+                translatedKey = '$schema';
+                break;
+            case 'jsonSchemaId':
+                translatedKey = '$id';
+                break;
+            case 'jsonSchemaRef':
+                translatedKey = '$ref';
+                break;
+        }
+        return translatedKey;
+    }
 }
 
 module.exports = SchemaService;
