5b8c57454d705b21338293c6960a7b0e240e5b6b
Tim Hardy <tim.hardy@omegacreations.com>
2020-07-02 08:57:43 -0500

    fixed missing pureMongoService

--- server/src/schedules/schedulesController.js
+++ server/src/schedules/schedulesController.js
@@ -3,6 +3,7 @@ import ScheduleService from './scheduleService.js';
 import authHelper from '../common/authHelper.js';
 import current from '../common/current.js';
 import SchemaService from '../server/graphQL/schemaService.js';
+import pureMongoService from '../database/pureMongoService.js';
 
 class SchedulesController {
     constructor(app) {
