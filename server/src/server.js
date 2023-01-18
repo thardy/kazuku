c62dc150e19489559ed55c014271354eac033d24
Tim Hardy <tim.hardy@omegacreations.com>
2023-01-18 09:20:24 -0600

    in progress

--- server/src/server.js
+++ server/src/server.js
@@ -70,9 +70,10 @@ async function startServer(config) {
     main.set('port', config.port || 3001);
 
 // serve static files out of this folder - referenced as /css, /img, /js
-    console.log('about to load static middleware');
-    main.use(express.static(global.appRoot + '/public', {extensions:['html']}));
-    main.use(bodyParser.json());
+    // todo: archChangesForDocker
+//     console.log('about to load static middleware');
+//     main.use(express.static(global.appRoot + '/public', {extensions:['html']}));
+//     main.use(bodyParser.json());
 
     main.use(setupAuthZone);
     main.use(cookieParser());
@@ -93,6 +94,39 @@ async function startServer(config) {
             }
         }));
     }
+
+    // todo: super temporary!!!
+    main.get('/', (req, res) => {
+        res.send('Hi');
+    });
+    main.get('/api/products', async (req, res) => {
+        const products = [
+            {
+                name: "Widget",
+                description: "It's a Widget.",
+                price: 19.95,
+                quantity: 100,
+                createdDate: "2022-12-19T10:04:58-06:00"
+            },
+            {
+                name: "Doohicky",
+                description: "The best, like, ever.",
+                price: 29.95,
+                quantity: 20,
+                createdDate: "2022-07-05T10:04:58-06:00"
+            },
+            {
+                name: "Thingamajig",
+                description: "No one know what this is.",
+                price: 99.99,
+                quantity: 43,
+                createdDate: "2022-03-01T10:04:58-06:00"
+            }
+
+        ];
+
+        res.send(products);
+    });
 
 // Map the routes - this creates the controllers, and routes are mapped in each controller via the mapRoutes function called in each constructor
     routes(main);
@@ -127,44 +161,44 @@ async function startServer(config) {
     });
 
 // site app - maps subdomains to site folders
-    let siteApp = express();
-    // Setup GraphQL for all subdomains
+    // todo: archChangesForDocker
+//     let siteApp = express();
+//     siteApp.use(cors());
+//
+//     const apiApolloServer = createApolloServer();
+//     apiApolloServer.applyGraphQlApiMiddleware({
+//         app: siteApp,
+//         //path: `http://thanosblog.kazuku.com:3001/graphql`, //'/graphql', //`http://kazuku.com:3001/graphql`,
+//         path: '/graphql-api',
+//         db: db,
+//     });
+//
+//     siteApp.use((req, res) => {
+//         const siteCode = req.vhost[0];
+//         req.originalUrl = req.url;
+//         let extension = '';
+//         if (!req.url.endsWith('.html')) {
+//             extension = '.html';
+//         }
+//         res.sendFile(__dirname + '/siteContent/' + siteCode + req.url + extension);
+//     });
 
-    // apolloServer.applyGraphQlPlaygroundMiddleware({
-    //     app: siteApp,
-    //     path: '/graphql', //`http://thanosblog.kazuku.com:3001/graphql`,
+    // todo: archChangesForDocker
+// Vhost app
+    // let app = express();
+    //
+    // app.use(vhost(config.hostname, main)); // Serves top level domain via main server app
+    // app.use(vhost(`*.${config.hostname}`, siteApp)); // Serves all subdomains via siteApp
+    //
+    //
+    // // listen on port config.port
+    // app.listen(config.port, () => {
+    //     console.log(`kazuku server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
     // });
 
-    siteApp.use(cors());
-
-    const apiApolloServer = createApolloServer();
-    apiApolloServer.applyGraphQlApiMiddleware({
-        app: siteApp,
-        //path: `http://thanosblog.kazuku.com:3001/graphql`, //'/graphql', //`http://kazuku.com:3001/graphql`,
-        path: '/graphql-api',
-        db: db,
-    });
-
-    siteApp.use((req, res) => {
-        const siteCode = req.vhost[0];
-        req.originalUrl = req.url;
-        let extension = '';
-        if (!req.url.endsWith('.html')) {
-            extension = '.html';
-        }
-        res.sendFile(__dirname + '/siteContent/' + siteCode + req.url + extension);
-    });
-
-// Vhost app
-    let app = express();
-
-    app.use(vhost(config.hostname, main)); // Serves top level domain via main server app
-    app.use(vhost(`*.${config.hostname}`, siteApp)); // Serves all subdomains via siteApp
-
-
-    // listen on port config.port
-    app.listen(config.port, () => {
-        console.log(`kazuku server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+    // this changes server.js to simply serve the api and the playground on the configured port
+    main.listen(config.port, (err) => {
+        console.log(`kazuku api listening on port ${config.port} (${config.env})`);
     });
 }
 
