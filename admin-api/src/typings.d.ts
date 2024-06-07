3c4468db868e6823bba96c1772f3508ec01ebd4a
Tim Hardy <tim.hardy@omegacreations.com>
2024-06-07 13:33:24 -0500

    deleted unused file

--- admin-api/src/typings.d.ts
+++
@@ -1,4 +0,0 @@
-// This is where we're putting all the third party modules that are giving us typing trouble that we don't care too much
-//  about their typings. Fighting their random typing issues is far outweighing any benefits we get from having their typings.
-declare module 'cookie-parser';
-declare module 'jsonwebtoken';
